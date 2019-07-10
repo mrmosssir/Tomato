06eeb80513f20981c9f097007e6719629c837db1
SuTo <suto@SuTode-MacBook-Pro.local>
2019-07-10 20:28:24 +0800

    fix

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/Tomata/dist',
+    assetsPublicPath: '/Tomato/dist',
 
     /**
      * Source Maps
